eb32afe199746a140e3d5b9c356394189ca083eb
Nguyễn Hữu Tứ <95615938+nguyenhuutu270402@users.noreply.github.com>
2023-03-22 19:51:55 +0700

    update 17

--- src/utilities/constants.js
+++ src/utilities/constants.js
@@ -1,4 +1,4 @@
 export const TOKEN_KEY = 'song bat dau tu dau';
-export const HOSTING = 'http://192.168.1.29:3000/';
-// export const HOSTING = 'https://app-comic-reading.onrender.com/';
+// export const HOSTING = 'http://192.168.1.29:3000/';
+export const HOSTING = 'https://app-movie-uatching.onrender.com/';
 
